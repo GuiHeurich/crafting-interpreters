4c209309dcab340ad0e949cd53ca901872da503f
Gui Heurich <guilherme.heurich@protonmail.com>
2024-06-20 10:26:05 +0100

    Implement function return statement

--- java/lox/Interpreter.java
+++ java/lox/Interpreter.java
@@ -66,6 +66,14 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Void visitiReturnStmt(Stmt.Return stmt) {
+        Object value = null;
+        if (stmt.value != null) value = evaluate(stmt.value);
+
+	throw new Return(value);
+    }
+
+    @Override
     public Void visitVarStmt(Stmt.Var stmt) {
         Object value = null;
         if (stmt.initializer != null) {
