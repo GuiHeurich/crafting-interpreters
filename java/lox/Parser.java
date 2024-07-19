2ba7452de802fcc7980dc0921ef86b553c14fb60
Gui Heurich <guilherme.heurich@protonmail.com>
2024-07-19 15:52:02 +0100

    Remove sun tools Expression resolver

--- java/lox/Parser.java
+++ java/lox/Parser.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import sun.tools.jstat.ExpressionResolver;
-
 import static com.crafting_interpreters.java.lox.TokenType.*;
 
 class Parser {
