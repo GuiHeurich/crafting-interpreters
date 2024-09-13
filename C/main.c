f99bc565faee387115588a03fdf13701d66ef59a
Gui Heurich <guilherme.heurich@protonmail.com>
2024-09-13 14:57:00 +0100

    Finish chapter

--- C/main.c
+++ C/main.c
@@ -5,7 +5,18 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+
+//    int constant = addConstant(&chunk, 1.2);
+//    writeChunk(&chunk, OP_CONSTANT, 123);
+//    writeChunk(&chunk, constant, 123);
+
+    for(int i = 0; i < 256; i++) {
+        int loop_constant = addConstant(&chunk, i);
+        writeChunk(&chunk, OP_CONSTANT, 123);
+        writeChunk(&chunk, loop_constant, 123);
+    }
+
+    writeChunk(&chunk, OP_RETURN, 123);
 
     dissassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
