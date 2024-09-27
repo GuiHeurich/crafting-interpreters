3320dbf6f8813bb787486915b13fc36aa3d3fde8
Gui Heurich <guilherme.heurich@protonmail.com>
2024-09-27 13:07:24 +0100

    Implement virtual machine as compiler's backend

--- C/main.c
+++ C/main.c
@@ -1,24 +1,36 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
-//    int constant = addConstant(&chunk, 1.2);
-//    writeChunk(&chunk, OP_CONSTANT, 123);
-//    writeChunk(&chunk, constant, 123);
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-    for(int i = 0; i < 256; i++) {
-        int loop_constant = addConstant(&chunk, i);
-        writeChunk(&chunk, OP_CONSTANT, 123);
-        writeChunk(&chunk, loop_constant, 123);
-    }
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
-    dissassembleChunk(&chunk, "test chunk");
+    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
